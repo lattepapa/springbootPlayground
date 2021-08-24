24e29c0fb42443a23d7342dd453e3ae885886be1
trackwisely <trackwisely@yhgwonMacBookPro.wisetracker-dev>
2021-08-24 18:12:08 +0900

    add packages and test env

---
+++ src/main/java/com/fastercamp/springboot/Application.java
@@ -0,0 +1,11 @@
+package com.fastercamp.springboot;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+// SpringBoot 자동 설정, Spring Bean 읽기/생성 자동 설정
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
