55d09878d46a7141f1c7c9fcfd6b30e6c9b92d3a
trackwisely <trackwisely@yhgwonMacBookPro.wisetracker-dev>
2021-08-25 16:51:57 +0900

    add update and read functions

--- src/main/java/com/harder/springboot/web/PostsApiController.java
+++ src/main/java/com/harder/springboot/web/PostsApiController.java
@@ -1,10 +1,11 @@
 package com.harder.springboot.web;
+import com.harder.springboot.domain.posts.PostsRepository;
 import com.harder.springboot.service.posts.PostsService;
+import com.harder.springboot.web.dto.PostsResponseDto;
 import com.harder.springboot.web.dto.PostsSaveRequestDto;
+import com.harder.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 // 생성자를 직접 안쓰고 @RequiredArgsConstructor 어노테이션으로 떼우는 이유 :
@@ -14,10 +15,23 @@ public class PostsApiController {
 
     private final PostsService postsService;
 
+    // 1. 데이터 등록기능
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
     }
     // @RequestBody 어노테이션은 해당 클래스를 객체로 생성해준다. 대신, 각 변수별로 데이터를 저장하는 것은 불가능하다
     // @RequestParam 어노테이션은 반면에 각 변수별로 데이터를 저장할 수 있게 해주는 대신, 클래스를 객체로 생성하는 것은 불가능하다
+
+    // 2. 데이터 수정기능
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    // 3. 데이터 조회기능
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id) {
+        return postsService.findById(id);
+    }
 }
