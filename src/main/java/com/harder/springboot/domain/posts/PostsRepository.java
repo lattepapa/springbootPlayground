574e5627bd78609c6cbd0ad3322322bef4e0d220
trackwisely <trackwisely@yhgwonMacBookPro.wisetracker-dev>
2021-08-26 10:40:40 +0900

    add Query to repository

--- src/main/java/com/harder/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/harder/springboot/domain/posts/PostsRepository.java
@@ -1,7 +1,13 @@
 package com.harder.springboot.domain.posts;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
 
 // JpaRepository는 MyBatis에서의 Dao와 같은 DB Layer 접근자(interface)이다 -> Posts 클래스로 DB 접근할 수 있게 해준다
