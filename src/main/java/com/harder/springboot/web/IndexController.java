874a93510cff68db9a5daac88526efa66a1d9a43
trackwisely <trackwisely@yhgwonMacBookPro.wisetracker-dev>
2021-08-26 10:15:21 +0900

    add board ui via bootstrap

--- src/main/java/com/harder/springboot/web/IndexController.java
+++ src/main/java/com/harder/springboot/web/IndexController.java
@@ -12,4 +12,10 @@ public class IndexController {
         // mustache 템플릿 엔진에 의해 String 값의 뒤에 .mustache 확장자가 붙어서
         // src > main > resources > templates 경로에 파일로 전환 및 저장되고, 이것을 View Resolver가 처리
     }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+        // 마찬가지로, "/posts/save"를 호출 시 posts-save.mustache를 호출하도록 메서드 구성
+    }
 }
