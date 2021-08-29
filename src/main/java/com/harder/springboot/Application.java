15095da6683a5bb0ecd8d382fe2905d33852dcd0
lattepapa <whitelatte46@gmail.com>
2021-08-29 15:06:16 +0900

    modify testing env and complete whole test pass

--- src/main/java/com/harder/springboot/Application.java
+++ src/main/java/com/harder/springboot/Application.java
@@ -3,8 +3,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
-// JPA Auditing 활성화
 @SpringBootApplication
 // SpringBoot 자동 설정, Spring Bean 읽기/생성 자동 설정
 public class Application {
--- src/test/java/com/harder/springboot/web/HelloControllerTest.java
+++ src/test/java/com/harder/springboot/web/HelloControllerTest.java
@@ -1,8 +1,12 @@
 package com.harder.springboot.web;
+import com.harder.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -14,7 +18,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 // 테스트 진행 시 JUnit 내장 실행자 외 다른 실행자도 실행
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class,
+excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+})
 // 여러 Spring 테스트 어노테이션 중 Web(Spring MVC)에 집중할 수 있는 어노테이션으로, @Controller, @ControllerAdvice 등을 사용가능
 public class HelloControllerTest {
 
@@ -23,6 +30,7 @@ public class HelloControllerTest {
     private MockMvc mvc;
     // Spring MVC 테스트의 시작점이며, HTTP(Web)의 다양한 메소드들을 테스트 가능
 
+    @WithMockUser(roles = "USER")
     @Test
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
@@ -34,6 +42,7 @@ public class HelloControllerTest {
         // 이에 따라 .andExpect()를 통해 이 mvc.perform의 결과를 다양하게 검증 가능
     }
 
+    @WithMockUser(roles = "USER")
     @Test
     public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
