4d45021716837458e7a79ea545072bb33602d639
lattepapa <whitelatte46@gmail.com>
2021-09-13 11:54:47 +0900

    complete ArrayList

---
+++ src/main/java/com/faster/javas/p2/wrapup4/Subject.java
@@ -0,0 +1,24 @@
+package com.faster.javas.p2.wrapup4;
+
+public class Subject {
+    // 속성
+    private String subjectName;
+    private int subjectScore;
+
+    // Getter & Setter
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
+    public int getSubjectScore() {
+        return subjectScore;
+    }
+
+    public void setSubjectScore(int subjectScore) {
+        this.subjectScore = subjectScore;
+    }
+}
